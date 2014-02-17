bb1a0ace66235b87060e31397a670446c6bf8616
= <=>
2014-02-16 21:09:54 -0500

    Tweeks

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ function include(file) {
   script.type = 'text/javascript';
   script.defer = true;
 
+  console.log($(document).find('head'));
   document.getElementsByTagName('head').item(0).appendChild(script);
   
 }
 
-/* include any js files here */
-include('jquery.autoloadtabs.js');
+/* include any js files here */include('jquery.autoloadtabs.js');
